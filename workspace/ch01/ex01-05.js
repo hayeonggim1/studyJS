2735857dd5681c79a79ed9eeabeb3abd7aaa6254
hayeonggim1 <hayeonggim1@gmail.com>
2025-03-31 10:34:23 +0900

    0331

---
+++ workspace/ch01/ex01-05.js
@@ -0,0 +1,19 @@
+/*
+변수 선언
+*/
+
+var a;
+var b, c;
+var d = 10;
+var e = 100,
+  f = 200;
+
+var user1 = "하츄핑";
+var user2 = "아자핑";
+
+var message1 = "아자핑이 '안녕하세요' 라고 말했다";
+var message2 = '아자핑이 "안녕하세요"라고 말했다';
+var message3 = "아자핑이 '안녕하세요' 라고 말했다 ";
+var message4 = "몰라핑이 \n모른다고 말\t했다";
+
+console.log(message1, message2, message3, message4);
