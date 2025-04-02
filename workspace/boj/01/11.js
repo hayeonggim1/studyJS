1f4a52d156519bd84b6c07fe9e78c28421c0b84a
hayeonggim1 <hayeonggim1@gmail.com>
2025-04-02 09:24:33 +0900

    0401-2

--- workspace/boj/01/11.js
+++ workspace/boj/01/11.js
@@ -24,3 +24,5 @@ const input = fs.readFileSync(0).toString().trim().split(" ");
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
 const c = parseInt(input[2]);
+
+console.log(a + b + c);
