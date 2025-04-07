ab24131db264ba51d5b1584972577d1344cb889c
hayeonggim1 <hayeonggim1@gmail.com>
2025-04-07 09:20:30 +0900

    0406

--- workspace/boj/03/06.js
+++ workspace/boj/03/06.js
@@ -1,68 +1,72 @@
 /*
-제목: 오븐 시계
-설명: 범위가 더 넓은 시간 계산 문제
-제출: https://www.acmicpc.net/submit/2525
+제목: 빠른 A+B
+설명: 빠르게 입력받고 출력하는 문제
+제출: https://www.acmicpc.net/submit/15552
 
 문제
-KOI 전자에서는 건강에 좋고 맛있는 훈제오리구이 요리를 간편하게 만드는 인공지능 오븐을 개발하려고 한다. 
-인공지능 오븐을 사용하는 방법은 적당한 양의 오리 훈제 재료를 인공지능 오븐에 넣으면 된다. 
-그러면 인공지능 오븐은 오븐구이가 끝나는 시간을 분 단위로 자동적으로 계산한다.
+본격적으로 for문 문제를 풀기 전에 주의해야 할 점이 있다. 
+입출력 방식이 느리면 여러 줄을 입력받거나 출력할 때 시간초과가 날 수 있다는 점이다.
 
-또한, KOI 전자의 인공지능 오븐 앞면에는 사용자에게 훈제오리구이 요리가 끝나는 시각을 알려 주는 디지털 시계가 있다.
+C++을 사용하고 있고 cin/cout을 사용하고자 한다면, 
+cin.tie(NULL)과 sync_with_stdio(false)를 둘 다 적용해 주고, 
+endl 대신 개행문자(\n)를 쓰자. 
+단, 이렇게 하면 더 이상 scanf/printf/puts/getchar/putchar 등 C의 입출력 방식을 사용하면 안 된다.
 
-훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때, 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
+Java를 사용하고 있다면, Scanner와 System.out.println 대신 BufferedReader와 BufferedWriter를 사용할 수 있다. 
+BufferedWriter.flush는 맨 마지막에 한 번만 하면 된다.
+
+Python을 사용하고 있다면, input 대신 sys.stdin.readline을 사용할 수 있다. 
+단, 이때는 맨 끝의 개행문자까지 같이 입력받기 때문에 문자열을 저장하고 싶을 경우 .rstrip()을 추가로 해 주는 것이 좋다.
+
+또한 입력과 출력 스트림은 별개이므로, 테스트케이스를 전부 입력받아서 저장한 뒤 전부 출력할 필요는 없다. 
+테스트케이스를 하나 받은 뒤 하나 출력해도 된다.
+
+자세한 설명 및 다른 언어의 경우는 이 글에 설명되어 있다.
+(https://docs.google.com/document/d/17OUl9nU9i7vTkhk2q_qy4Q5Vl0HHE9bTLUHwbLp56WM/edit?tab=t.0#heading=h.mwvd9fqamd6)
+이 블로그 글에서 BOJ의 기타 여러 가지 팁을 볼 수 있다.
+(https://www.acmicpc.net/blog/view/55)
+언어별 제한 시간 참고
+(https://help.acmicpc.net/language/info)
 
 입력
-첫째 줄에는 현재 시각이 나온다. 현재 시각은 시 A (0 ≤ A ≤ 23) 와 분 B (0 ≤ B ≤ 59)가 정수로 빈칸을 사이에 두고 순서대로 주어진다. 
-두 번째 줄에는 요리하는 데 필요한 시간 C (0 ≤ C ≤ 1,000)가 분 단위로 주어진다.
+첫 줄에 테스트케이스의 개수 T가 주어진다. T는 최대 1,000,000이다. 
+다음 T줄에는 각각 두 정수 A와 B가 주어진다. A와 B는 1 이상, 1,000 이하이다.
 
 출력
-첫째 줄에 종료되는 시각의 시와 분을 공백을 사이에 두고 출력한다. 
-(단, 시는 0부터 23까지의 정수, 분은 0부터 59까지의 정수이다. 디지털 시계는 23시 59분에서 1분이 지나면 0시 0분이 된다.)
+각 테스트케이스마다 A+B를 한 줄에 하나씩 순서대로 출력한다.
 
 예제 입력 1
-14 30
-20
+5
+1 1
+12 34
+5 500
+40 60
+1000 1000
+
 예제 출력 1
-14 50
-
-예제 입력 2
-17 40
-80
-예제 출력 2
-19 0
-
-예제 입력 3
-23 48
-25
-예제 출력 3
-0 13
+2
+46
+505
+100
+2000
 */
 
 const fs = require("fs");
-const during = fs.readFileSync(0).toString().trim().split("\n");
-const inputTime = during[0].split(" ");
+const input = fs.readFileSync(0, "utf8").toString().trim().split("\n");
+const num = parseInt(input[0]);
+let result = "";
 
-// hour
-let h = parseInt(inputTime[0]);
-// minute
-let m = parseInt(inputTime[1]);
-// during
-let d = parseInt(during[1]);
-
-// 분에 필요시간을 더한 값이 60을 넘지 않을 때
-if (m + d < 60) {
-  m = m + d;
-  console.log(h, m);
-
-  // 분에 필요시간을 더한 값이 60을 넘을 때
-} else {
-  m = m + d;
-  h = Math.floor(m / 60) + h;
-  m = m % 60;
-
-  if (h >= 24) {
-    h = h - 24;
-  }
-  console.log(h, m);
+for (let i = 1; i <= num; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  result += a + b + "\n";
 }
+
+console.log(result);
+
+// input[i]에 parseInt로 감싸면 안되는 이유: input[i] 자체가 문자열을 담기 떄문. 예를 들어 '3 4'라는 배열이 input[i]에 들어갈 경우 '3 4'-> '3'의 숫자만 남음.
+// for (let i = 1; i < num + 1; i++) {
+//   const two = input[i].split(" ");
+//   const a = parseInt(two[0]);
+//   const b = parseInt(two[1]);
+//   console.log(a + b);
+// }
