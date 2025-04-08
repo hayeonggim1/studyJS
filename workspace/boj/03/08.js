d5e2d53c9c5e3761bded0bacc7eae3d33b15e7cb
hayeonggim1 <hayeonggim1@gmail.com>
2025-04-08 15:45:34 +0900

    0408-1

--- workspace/boj/03/08.js
+++ workspace/boj/03/08.js
@@ -1,7 +1,7 @@
 /*
-제목: A+B - 7
-설명: A+B를 조금 더 아름답게 출력하는 문제
-제출: https://www.acmicpc.net/submit/11021
+제목: A+B - 8
+설명: A+B를 바로 위 문제보다 아름답게 출력하는 문제
+제출: https://www.acmicpc.net/submit/11022
 
 문제
 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
@@ -11,22 +11,22 @@
 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
 출력
-각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
 
 예제 입력 1
 5
-1 1codegi
+1 1
 2 3
 3 4
 9 8
 5 2
 
 예제 출력 1
-Case #1: 2
-Case #2: 5
-Case #3: 7
-Case #4: 17
-Case #5: 7
+Case #1: 1 + 1 = 2
+Case #2: 2 + 3 = 5
+Case #3: 3 + 4 = 7
+Case #4: 9 + 8 = 17
+Case #5: 5 + 2 = 7
 */
 
 const fs = require("fs");
@@ -38,5 +38,5 @@ for (let i = 1; i <= num; i++) {
   const a = parseInt(source[0]);
   const b = parseInt(source[1]);
   const seq = i;
-  console.log(`Case #${seq}:`, a + b);
+  console.log(`Case #${seq}: ${a} + ${b} = ${a + b}`);
 }
---
+++ workspace/boj/03/09.js
@@ -0,0 +1,36 @@
+/*
+제목: 별 찍기 - 1
+설명: 별을 찍는 문제 1
+제출: https://www.acmicpc.net/submit/2438
+
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+예제 입력 1
+5
+
+예제 출력 1
+*
+**
+***
+****
+*****
+*/
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+const nth = parseInt(input);
+
+for (let i = 0; i < nth; i++) {
+  let stars = "*";
+  for (let j = 0; j < i; j++) {
+    stars += "*";
+  }
+  console.log(stars);
+}
