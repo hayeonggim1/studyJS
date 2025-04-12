ebc27da3f9727ccd349c1ced2afe374d04a5e9d6
hayeonggim1 <hayeonggim1@gmail.com>
2025-04-12 17:11:39 +0900

    settings until 0412

--- workspace/ch01/ex01-28.js
+++ workspace/ch01/ex01-28.js
@@ -1,38 +1,49 @@
 /*
 유사 배열 객체
+- length 속성을 가지고 있음
+- 0부터 시작하는 인덱스를 가진 속성들을 포함
+- 배열처럼 동작하지만 실제 배열은 아님
 */
 
 /**
  * 전달받은 배열의 모든 요소를 인덱스와 함께 출력한다.
- * @param {any[]} arr - 배열 (any = 어떤 타입이든 상관없다)
+ * @param {any[]} arr - 배열
  */
-
 function printArr(arr) {
   for (let i = 0; i < arr.length; i++) {
     console.log(i, arr[i]);
   }
 }
 
-const colorArr = ["orange", "yelow", "green"];
-colorArr.push("black");
+// 일반 배열 사용 예시
+const colorArr = ["orange", "yellow", "green"];
+colorArr.push("black"); // 배열 끝에 'black' 추가
 printArr(colorArr);
 
+/*
+유사 배열 객체 예시
+- length 속성으로 길이를 관리
+- 0부터 시작하는 인덱스로 요소 접근
+- push 메서드를 직접 구현
+*/
 const arr = {
   length: 3,
   0: "orange",
   1: "yellow",
   2: "green",
   /**
-   * 맨 끝에 지정한 elem을 추가한다.
+   * 맨 끝에 지정한 elem를 추가한다.
    * @param {*} elem - 추가할 요소
    */
   push: function (elem) {
-    arr[arr.length] = elem;
-    arr.length++;
+    // TODO this 배운 후에
+    arr[arr.length] = elem; // 현재 length 위치에 요소 추가
+    arr.length++; // length 증가
   },
-}; // 객체
-arr.push("black");
-arr.push("white");
+};
+arr.push("black"); // 유사 배열에 'black' 추가
+arr.push("white"); // 유사 배열에 'white' 추가
 printArr(arr);
 
 // 문자열도 유사 배열 객체의 일종
+// printArr('이것은 문자열');
