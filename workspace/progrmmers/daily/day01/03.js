467a63441f2f4ecf1c88121f8b2037d2fad017ae
hayeonggim1 <hayeonggim1@gmail.com>
2025-04-28 09:17:24 +0900

    0428-1

--- workspace/progrmmers/daily/day01/03.js
+++ workspace/progrmmers/daily/day01/03.js
@@ -19,17 +19,27 @@ string 5
 stringstringstringstringstring
 */
 
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-let input = [];
+// let input = [];
 
-rl.on('line', function (line) {
-  input = line.split(' ');
-}).on('close', function () {
-  str = input[0];
-  n = Number(input[1]);
-});
+// rl.on('line', function (line) {
+//   input = line.split(' ');
+// }).on('close', function () {
+//   str = input[0];
+//   n = Number(input[1]);
+// });
+
+const fs = require("fs");
+const fileData = fs.readFileSync(0).toString().trim().split(" ");
+
+const word = fileData[0];
+const num = parseInt(fileData[1]);
+
+for(let i=0; i<num; i++){
+    console.log(word);
+}
--- workspace/progrmmers/daily/day01/04.js
+++ workspace/progrmmers/daily/day01/04.js
@@ -18,16 +18,32 @@ aBcDeFg
 AbCdEfG
 */
 
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const fs = require("fs");
+const fileData = fs.readFileSync(0).toString().trim();
 
-let input = [];
+const str = [];
+for(let i=0; i<fileData.length; i++){
+  str += fileData[i];
+}
 
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
-});
+for(let i=0; i<str.length; i++){
+  if(str[i] !== str[i].toUpperCase()){
+    str[i] = str[i].toUpperCase();
+  } else{
+    str[i] = str[i].toLowerCase();
+  }
+}
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//     input = [line];
+// }).on('close',function(){
+//     str = input[0];
+// });
