38a96e3cd3a2b0b5eb3c8841684efff0639698e2
hayeonggim1 <hayeonggim1@gmail.com>
2025-05-12 10:16:14 +0900

    0512

--- workspace/ch05/ajax/ex05-13.ts
+++ workspace/ch05/ajax/ex05-13.ts
@@ -1,3 +1,25 @@
 // 랜덤 고양이 이미지 가져오기 - Fetch API 사용
 // ex05-12.ts 복사
 
+import { type Cat } from "./types.js";
+import { appendImages } from "./utils.js";
+
+const url = 'https://api.thecatapi.com/v1/images/search';
+
+const btn = document.querySelector('button');
+
+btn?.addEventListener('click', getImages);
+
+async function getImages(){
+  try{
+  const response = await fetch(url);
+  if(response.ok){
+    const data: Cat[] = await response.json();
+    appendImages(data);
+  }else{
+    console.log(response.status, '에러 발생'); // status (응답 상태 코드)
+  }
+} catch(err){
+  console.log('에러 발생', err);
+}
+};
--- workspace/ch05/ajax/ex05-14.ts
+++ workspace/ch05/ajax/ex05-14.ts
@@ -1,3 +1,22 @@
 // 랜덤 고양이 이미지 가져오기 - axios 라이브러리
 // ex05-13.ts 복사
 
+import { type Cat } from "./types.js";
+import { appendImages } from "./utils.js";
+
+const url = 'https://api.thecatapi.com/v1/images/search';
+
+const btn = document.querySelector('button');
+
+btn?.addEventListener('click', getImages);
+
+async function getImages(){
+  try{
+  const response = await axios.get<Cat[]>(url);
+  const data = response.data;
+  appendImages(data);
+  
+} catch(err){ // 네트워크 오류
+  console.log('에러 발생', err);
+}
+};
